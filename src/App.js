97d662d17fb977232ce8b01e81ab551a9989c11d
rifafatima <rifafatima02@gmail.com>
2021-02-07 04:14:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import React from'react';
 import logo from './logo.svg';
 import './App.css';
+import './highcss.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  render()
+  {
+    return (
+      <div className="App">
+          <section className="cards-wrapper">
+    <div className="card-grid-space">
+      <a className="card"  href="" style={{backgroundImage: "url('./images/leaf1.jpg')"}}>
+        <div>
+          <h1>Exclusive tropical plants for your home!</h1>
+          <div className="tags">
+            <div className="tag"  href="#gg">Shop</div>                 
+          </div>
+        </div>
+      </a>
     </div>
-  );
+
+    <div className="card-grid-space">
+      <a className="card" href="" style={{backgroundImage: "url('./images/leaf2.jpg')"}}>
+        <div>
+          <h1>Rich fragrant flower pots and more!</h1>
+          <div className="tags">
+            <div className="tag"  href="#gg">Shop</div>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    <div className="card-grid-space">
+      <a className="card" href="" style={{backgroundImage: "url('./images/leaf3.jpg')"}}>
+        <div>
+          <h1>Exotic succulents for all indoor decor!</h1>
+          <div className="tags">
+            <div className="tag"  href="#gg">Shop</div>
+          </div>
+        </div>
+      </a>
+    </div>
+
+  </section>
+      </div>
+    );
+  }
 }
 
 export default App;
